48a4def2b2070082d75b5cc9fb9d54e954e788ec
gosuz <go.suz55@gmail.com>
2024-05-14 00:21:02 +0800

    updated users model by adding name to it, and did rails db:migrate. Also did delete one migration of AddedEmailToUser and AddedPasswordToUser, after doing rails db:rollback, and coming to a conclusion I don't need these two migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # associations
+  # a user has many plans
+  has_many :plans
+  # a user has many exercises through plans
+  has_many :exercises, through: :plans
+
+  # validations
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, :password, presence: true
 end
