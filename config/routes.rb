aad1e7d54a08980c087f6193d95d60264934728e
gosuz <go.suz55@gmail.com>
2024-05-14 18:01:08 +0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
 
   # As a user, I can see the get started (home page)
+  # # As a user I can create a workout plan
+  # post "/plans", to: "plans#create"
+  # # As a user, I can edit my response to the form (exercise clusters/ 1RM)
+  # get "/plans/:id/edit", to: "plans#edit"
+  # patch "/plans/:id", to: "plans#update"
+  # # As a user, I can view my workout plan
+  # get "/plans/:id", to: "plans#show"
+  # # As a user, I can delete my workout plan
+  # delete "/plans/:id", to: "plans#destroy"
   root to: "pages#home"
-
-  # As a user I can create a workout plan
-  # As a user, I can edit my response to the form (exercise clusters/ 1RM)
-  # As a user, I can view my workout plan
-  # As a user, I can delete my workout plan
-
+  resources :plans, only: [:new, :create, :edit, :update, :show, :destroy]
+  resources :exercises, only: [:new, :create, :index, :show, :destroy]do
+    resources :weights, onlu: [:new, :create, :index, :show, :destroy]
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
